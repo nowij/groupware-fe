03fab95a205648037e8b7ff84dce654176fd4f52
nowij <nowijdl@gmail.com>
2023-08-03 14:33:08 +0900

    mixins 설정 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
+import mixins from './mixins'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.use(mixins)
+app.mount('#app')
