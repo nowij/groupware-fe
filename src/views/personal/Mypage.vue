5200edf6f74dfaa5dcf33f0ba0c0484f60d98df3
nowij <nowijdl@gmail.com>
2023-08-21 13:37:14 +0900

    mypage 스크립트 setup으로 변경

--- src/views/personal/Mypage.vue
+++ src/views/personal/Mypage.vue
@@ -5,72 +5,77 @@
                 <div class="card">
                     <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
                         <img src="@/assets/profile.png" alt="Profile" class="rounded-circle">
-                        <h2>{{ employee.userName }}</h2>
-                        <h3>{{ positName }}</h3>
+                        <h2>{{ user.userName }}</h2>
+                        <h3>{{ myInfo.position.positionName }}</h3>
                     </div>
                 </div>
             </div>
-            
+
             <div class="col-xl-4">
                 <div class="card">
                     <div class="card-body pt-3">
                         <ul class="nav nav-tabs nav-tabs-bordered">
                             <li class="nav-item">
-                                <button class="nav-link" @click="overviewTabClick" :class="{active: overview}">기본정보</button>
+                                <button class="nav-link" @click="overviewTabClick"
+                                    :class="{ active: overview }">기본정보</button>
                             </li>
                             <li class="nav-item">
-                                <button class="nav-link" @click="passwdTabClick" :class="{active:changpw}">비밀번호 변경</button>
+                                <button class="nav-link" @click="passwdTabClick" :class="{ active: changpw }">비밀번호
+                                    변경</button>
                             </li>
                         </ul>
 
                         <div class="tab-content pt-2">
                             <!-- 기본정보 -->
                             <div class="tab-pane fade show active profile-overview" id="overview" v-if="overview">
-                                <form>
+                                <form >
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label ">사번</div>
-                                        <div class="col-lg-9 col-md-8">{{ employee.employeeId }}</div>
+                                        <div class="col-lg-9 col-md-8">{{ myInfo.employeeId }}</div>
                                     </div>
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">이름</div>
-                                        <div class="col-lg-9 col-md-8">{{ employee.userName }}</div>
+                                        <div class="col-lg-9 col-md-8">{{ myInfo.userName }}</div>
                                     </div>
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">소속</div>
-                                        <div class="col-lg-9 col-md-8">{{ deptName }}</div>
+                                        <div class="col-lg-9 col-md-8">{{ myInfo.department.deptName }}</div>
                                     </div>
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">직위</div>
-                                        <div class="col-lg-9 col-md-8">{{ positName }}</div>
+                                        <div class="col-lg-9 col-md-8">{{ myInfo.position.positionName }}</div>
                                     </div>
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">이메일</div>
                                         <div class="col-md-8 col-lg-9">
-                                            <input type="text" v-model="employee.email" class="form-control">
+                                            <input type="text" v-model="myInfo.email" class="form-control">
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">전화번호</div>
                                         <div class="col-md-8 col-lg-9">
-                                            <input type="text" v-model="employee.phone" class="form-control">
+                                            <input type="text" v-model="myInfo.phone" class="form-control">
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">주소</div>
                                         <div class="col-md-8 col-lg-9">
-                                            <input type="text" v-model="employee.address" class="form-control">
+                                            <input type="text" v-model="myInfo.address" class="form-control">
                                         </div>
                                     </div>
                                     <div class="text-center">
-                                        <button type="submit" class="btn btn-primary w-100" @click.self.prevent="submit">저장</button>
+                                        <button type="submit" class="btn btn-primary w-100"
+                                            @click.self.prevent="submit">저장</button>
                                     </div>
                                 </form>
                             </div>
                             <!-- 비밀번호 변경 -->
-                            <div class="tab-pane fade show active profile-overview" id="change-password" v-else-if="changpw">
+                            <div class="tab-pane fade show active profile-overview" id="change-password"
+                                v-else-if="changpw">
                                 <form>
                                     <div class="row mb-3">
-                                        <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">현재 비밀번호</label>
+                                        <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">현재
+                                            비밀번호</label>
                                         <div class="col-md-8 col-lg-9">
                                             <input name="password" type="password" class="form-control"
                                                 id="currentPassword">
@@ -102,66 +107,55 @@
     </section>
 </template>
 
-<script>
-import {  usePersonalStore, useAuthStore, useAlertStore } from '@/stores';
+<script setup>
+import { usePersonalStore, useAuthStore, useAlertStore } from '@/stores';
 import { storeToRefs } from 'pinia';
+import { ref, onMounted } from 'vue';
 
-export default {
-    data() {
-        return {
-            employee: {},
-            deptName: '',
-            positName: '',
-            overview : true,
-            changpw : false,
-        }
-    },
-    mounted() {
-        this.select();
-    },
-    methods: {
-        async select() {
-            const personalStore = usePersonalStore();
-            const authStore = useAuthStore();
-            const { user } = storeToRefs(authStore);
-            const { myInfo } = storeToRefs(personalStore);
-            const employeeId = user;
-            const datas = {
-                employeeId: employeeId.value.employeeId
-            }
-            
-            await personalStore.getMyInfo(datas);
-            this.employee = myInfo;
-            this.positName = this.employee.position.positionName;
-            this.deptName = this.employee.department.deptName;
-            
-        },
-        submit() {
-            const personalStore = usePersonalStore();
-            const alertStore = useAlertStore();
-            const { status } = storeToRefs(personalStore);
-            const datas = {
-                employeeId: this.employee.employeeId,
-                email: this.employee.email,
-                phone: this.employee.phone,
-                address: this.employee.address
-            }
-            personalStore.saveMyInfo(datas);
-            console.log(status.value);
-            if (status.value === 200) {
-                alertStore.success('저장 되었습니다.');
-            } else {
-                alertStore.error('오류가 발생했습니다.');
-            }
-        },
-        overviewTabClick() {
-            this.overview = true;
-            this.changpw = false;
-        },
-        passwdTabClick() {
-            this.overview = false;
-            this.changpw = true;
-        }
+const personalStore = usePersonalStore()
+const authStore = useAuthStore()
+const alertStore = useAlertStore()
+const { status } = storeToRefs(personalStore)
+const { user } = storeToRefs(authStore)
+const { myInfo } = storeToRefs(personalStore)
+const overview = ref(true)
+const changpw = ref(false)
+
+onMounted(() => {
+    select()
+})
+
+// methods
+const select = async () => {
+    const datas = {
+        employeeId: user.value.employeeId
+    }
+    await personalStore.getMyInfo(datas);
+}
+
+const submit = async () => {
+    const datas = {
+        employeeId: myInfo.value.employeeId,
+        email: myInfo.value.email,
+        phone: myInfo.value.phone,
+        address: myInfo.value.address
+    }
+    await personalStore.saveMyInfo(datas);
+
+    if (status.value === 200) {
+        alertStore.success('저장 되었습니다.');
+    } else {
+        alertStore.error('오류가 발생했습니다.');
     }
 }
+
+const overviewTabClick = () => {
+    overview.value = true;
+    changpw.value = false;
+}
+const passwdTabClick = () => {
+    overview.value = false;
+    changpw.value = true;
+}
+
 </script>
